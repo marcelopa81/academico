0e671618d5e805dbe86d143be608ea8dcd92871f
Marcelo Peixoto de Abreu <marcelopeixotodeabreu@gmail.com>
2024-10-02 13:26:55 -0300

    Implementação dos recursos que inserem, alteram e deletam disciplinas

--- src/main/java/br/edu/ifgoias/academico/recursos/DisciplinaRecurso.java
+++ src/main/java/br/edu/ifgoias/academico/recursos/DisciplinaRecurso.java
@@ -3,11 +3,18 @@ package br.edu.ifgoias.academico.recursos;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifgoias.academico.entities.Curso;
@@ -33,4 +40,25 @@ public class DisciplinaRecurso {
 		Disciplina disciplina = servico.findById(id);
 		return ResponseEntity.ok().body(disciplina);
 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Disciplina> insert (@RequestBody Disciplina disciplina){
+		disciplina = servico.insert(disciplina);
+		return ResponseEntity.ok().body(disciplina);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete (@PathVariable Integer id) {
+		servico.delete(id);
+	}
+	
+	@PutMapping(value = "/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void update (@PathVariable Integer id, @RequestBody Disciplina disciplina) {
+		servico.update(id, disciplina);
+	}
+	
+	
 }
